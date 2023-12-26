e9542bd206e4a45e4078a9605b42b6a855d919b6
sir-pinski <anthony.pecorella@gmail.com>
2023-12-25 22:25:11 -0500

    Updatetest

--- src/data/projEntry.tsx
+++ src/data/projEntry.tsx
@@ -1,8 +1,16 @@
 import {BaseLayer, createLayer} from "../game/layers";
+import {createResource} from "../features/resources/resource";
+import {DecimalSource} from "../lib/break_eternity";
+import MainDisplay from "../features/resources/MainDisplay.vue";
 
 const id = "Practice";
 const layer = createLayer(id, function (this: BaseLayer) {
+    const PracticePoints = createResource<DecimalSource>(0, "Practice Points")
     return {
-        display: jsx(() => <>Practice Layer</>)
+        display: jsx(() => (
+            <>
+                <MainDisplay resource={PracticePoints} />
+            </>
+        ))
     };
 });
